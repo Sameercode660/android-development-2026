1f243dabf96ef0026e20e93032ebae949dc18c8e
Mohd Sameer <mesh789736@gamil.com>
2026-02-26 15:20:12 +0530

    implicit intent, scrollview and imageview

--- Eighth_application/app/src/main/java/com/example/eighth_application/MainActivity.kt
+++ Eighth_application/app/src/main/java/com/example/eighth_application/MainActivity.kt
@@ -1,21 +1,24 @@
-package com.example.seventh_application
+package com.example.eighth_application
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
+        val dialButton = findViewById<Button>(R.id.dialButton)
 
-        button.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
+        dialButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = "tel:+916394867435".toUri()
             startActivity(intent)
         }
     }
---
+++ Seventh_application/app/src/main/java/com/example/seventh_application/SignUpActivity.kt
@@ -0,0 +1,16 @@
+package com.example.seventh_application
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_signup)
+
+    }
+}
